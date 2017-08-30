255a8d507abfc3f46c09c7fd84c792435b72e635
Jaybird9er <JameyKirk@gmail.com>
2017-08-30 20:59:14 +0900

    Stuck!!

--- js/main.js
+++ js/main.js
@@ -11,10 +11,7 @@ $('.student-item').attr('value', function(i) {
 
 // 03. Loop through student list and display (10 per page/1st 10)
 for (let i = 0; i < 10; i++) {
-	$('.student-item')[i];
-	if ( '.student-item'[i] < 10 ) {
-		'.student-item'[i].push('.student-list');
-	}
+	$('.student-item')[0][i];
 }
 
 
