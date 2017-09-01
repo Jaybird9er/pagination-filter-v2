53fa19b1a4f36fc8a03f966a3659279f2456bbf7
Jaybird9er <JameyKirk@gmail.com>
2017-09-01 20:56:23 +0900

    Cleaned up after solving showPage function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 /* Hide First 10 Students */
 const studentList = document.querySelectorAll('.student-item');
 
+// showPage removes entire list and then repopulates with first 10 students.
 const showPage = ( pageNum, listOfStudents ) => {
+	// Removes entire list
 	$(studentList).css('display', 'none');
+	// 
 	for (let i = 0; i < 10; i++) {
 		let list = document.querySelectorAll('.student-item')[i];
 		$(list).css('display', 'block');
@@ -10,22 +13,5 @@ const showPage = ( pageNum, listOfStudents ) => {
 }
 
 $(showPage);
-// 01. Hide All Students
-//$(studentList).css('display', 'none');
-
-/*// 02. Assign values to each student list element
-$('.student-item').attr('value', function(i) {
-	return i++;
-});*/
-
-// 03. Loop through student list and display (10 per page/1st 10)
-/*for (let i = 0; i < 10; i++) {
-	let list = document.querySelectorAll('.student-item')[i];
-	$(list).css('display', 'block');
-	//$(studentList).show().attr('value', function(i){});
-}
-
-
-*/
 
 
