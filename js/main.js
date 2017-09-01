9c2396cb03bab641e440ed78fbfd8610c10eb4c3
Jaybird9er <JameyKirk@gmail.com>
2017-09-01 20:51:15 +0900

    Finally got the showPage function sorted!!!

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
 /* Hide First 10 Students */
 const studentList = document.querySelectorAll('.student-item');
 
-// 01. Hide All Students
-$('.student-item').hide();
-
-// 02. Assign values to each student list element
-$('.student-item').attr('value', function(i) {
-	return i++;
-});
-
-// 03. Loop through student list and display (10 per page/1st 10)
-for (let i = 0; i < 10; i++) {
-	$('.student-item')[0][i];
+const showPage = ( pageNum, listOfStudents ) => {
+	$(studentList).css('display', 'none');
+	for (let i = 0; i < 10; i++) {
+		let list = document.querySelectorAll('.student-item')[i];
+		$(list).css('display', 'block');
+	}
 }
 
+$(showPage);
+// 01. Hide All Students
+//$(studentList).css('display', 'none');
+
+/*// 02. Assign values to each student list element
+$('.student-item').attr('value', function(i) {
+	return i++;
+});*/
+
+// 03. Loop through student list and display (10 per page/1st 10)
+/*for (let i = 0; i < 10; i++) {
+	let list = document.querySelectorAll('.student-item')[i];
+	$(list).css('display', 'block');
+	//$(studentList).show().attr('value', function(i){});
+}
+
+
+*/
 
 
